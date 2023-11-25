76b46a21fb7cdf01fd67cb782fdbb47732281093
Alexey <nomer9122345680@gmail.com>
2023-11-25 17:28:28 +0500

    Add section Blog and Footer. End of create layout.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
-$(function () {
-
-})
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 2,
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
